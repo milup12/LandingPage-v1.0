35cc7ee6d1a44dc51867c4bfe0020c7cc27da8e9
Camila Peña <acpenacturismo@gmail.com>
2023-05-16 21:19:14 -0500

    Cambios color

--- dato.js
+++ dato.js
@@ -3,9 +3,9 @@ function capturarDatos(){
   const correo = document.getElementById('correo').value;
   const telefono = document.getElementById('telefono').value;
 
-  console.log('nombre',nombre);
-  console.log('correo', correo);
-  console.log('telefono',telefono);
+  console.log(nombre);
+  console.log(correo);
+  console.log(telefono);
 }
 
 // capturarDatos()
